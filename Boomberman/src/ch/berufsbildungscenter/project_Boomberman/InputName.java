317df33cb5c66e837d1ce1102b585a76b8ee3efd
zjannn <zjannn@WS-ZJANNN.bbcnet-zh.local>
2018-01-31 11:24:11 +0100

    inputname fertig

--- Boomberman/src/ch/berufsbildungscenter/project_Boomberman/InputName.java
+++ Boomberman/src/ch/berufsbildungscenter/project_Boomberman/InputName.java
@@ -1,37 +1,33 @@
 package ch.berufsbildungscenter.project_Boomberman;
 
-
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class InputName extends JDialog {
-	
+
 	private JTextField demand = new JTextField("Gib deinen Spielnamen ein und drücke auf 'Start'!");
 	private JTextField playerName = new JTextField();
 	private JButton startButton = new JButton("Start");
+	private String player1Name;
 
 	public static void main(String[] args) {
-		
+
 		InputName inputName = new InputName();
-		inputName.setSize(550,300);
+		inputName.setSize(550, 300);
 		inputName.setVisible(true);
 		inputName.setResizable(false);
-		
-		
-		/*JTextField playerName = new JTextField();
-		playerName.setFont(new Font("Arial",Font.BOLD,24));
-		Object[] message = {"Spielname", playerName};
-		
-		JOptionPane pane = new JOptionPane(message, JOptionPane.PLAIN_MESSAGE, JOptionPane.OK_CANCEL_OPTION);
-		pane.createDialog(null, " ").setVisible(true);
-		System.out.println(playerName.getText());*/
+
 	}
-	
+
 	public InputName() {
 		setTitle(" ");
 		
@@ -43,9 +39,17 @@ public class InputName extends JDialog {
 		demand.setFont(new Font("Arial", Font.TRUETYPE_FONT, 24));
 		demand.setEditable(false);
 		
+		
 		add(demand, BorderLayout.NORTH);
 		add(startButton, BorderLayout.SOUTH);
 		add(playerName, BorderLayout.CENTER);
+		
+		startButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				player1Name = playerName.getText();
+				System.out.println(player1Name);
+			}
+		});
 	}
 
 }
