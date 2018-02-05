69efc44a506fbd6ba3384b946dacdeb87e3e2dd8
zsciaj <zsciaj@WS-ZSCIAJ.bbcnet-zh.local>
2018-02-05 10:40:15 +0100

    TIMER

--- Boomberman/src/ch/berufsbildungscenter/project_Boomberman/Window.java
+++ Boomberman/src/ch/berufsbildungscenter/project_Boomberman/Window.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Window extends JFrame{
 	
@@ -15,10 +16,20 @@ public class Window extends JFrame{
 	
 	public static void main(String[] args) {
 		Window w = new Window();
+		
 		Map m = new Map(w);
 		m.show(m.load("map1"));
 		
-
+		
+		
+		
+		Timer t = new Timer();
+		Thread th = new Thread(t);
+		th.start();
+		
+		
+		
+		w.add(t,BorderLayout.NORTH);
 		w.add(m,BorderLayout.SOUTH);
 		w.setSize((int)m.getSize().getWidth(),(int)m.getSize().getHeight());
 		w.setVisible(true);
