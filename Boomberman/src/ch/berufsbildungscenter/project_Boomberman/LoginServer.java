448e310b049696cf1b77880884a049b85c16013e
zsciaj <zsciaj@WS-ZSCIAJ.bbcnet-zh.local>
2018-02-05 14:20:42 +0100

    o

--- Boomberman/src/ch/berufsbildungscenter/project_Boomberman/LoginServer.java
+++ Boomberman/src/ch/berufsbildungscenter/project_Boomberman/LoginServer.java
@@ -5,7 +5,7 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class LoginServer {
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 		try {
 			// start RMI registry on default port 1099
 			Registry reg = LocateRegistry.createRegistry(1099);
